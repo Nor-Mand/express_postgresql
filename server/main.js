1b2afe07c35a09c2a62227b66706997d5cc87682
Normand Terceros <normandlaredo@gmail.com>
2022-02-12 01:02:32 +0000

    Start the project

--- server/main.js
+++ server/main.js
@@ -14,7 +14,7 @@ app.use(express.json());
 // FETCH  ALL DATA
 app.get("/testing", async (req,res) => {
     try {
-        const allData = await pool.query('SELECT * FROM res_partner')
+        const allData = await pool.query('SELECT id, name FROM res_partner LIMIT 10')
         res.json(allData.rows);
     } catch (err) {
         console.log(err.message)
@@ -25,7 +25,7 @@ app.get("/testing", async (req,res) => {
 app.get("/testing/:id", async (req,res) => {
     try {
         const { id } = req.params;
-        const idData = await pool.query('SELECT * FROM res_partner WHERE id = $1', [id])
+        const idData = await pool.query('SELECT id, name FROM res_partner WHERE id = $1', [id])
         res.json(idData.rows[0]);
     } catch (err){
         console.log(err.message)
